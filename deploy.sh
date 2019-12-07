015312b0a06969015f3bb19be9f628b0f539f93f
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2019-12-07 17:42:00 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -6,31 +6,33 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 user_name="momotaro98"
 
-# Go To Public folder
+echo 'Go To Public folder'
 cd public
 
-# Set Git configurations
+ls -la
+
+echo 'Set Git configurations'
 git config --local user.name $user_name
 git config --local user.email ${EMAIL_ADDRESS}
 
 git remote set-url origin https://$user_name:${DEPLOY_TOKEN}@github.com/$user_name/$user_name.github.io.git
 
-# Add changes to git.
+echo 'Add changes to git.'
 git checkout -b master
 git add .
 
-# Commit changes
+echo 'Commit changes'
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
-# Push
+echo 'Push'
 git push origin HEAD
 
 
-# Back from public
+echo 'Back from public'
 cd ..
 git checkout -b master
 git add public
