1aee18a13e9688d2f7418eb985a01c9c1712dcef
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2019-12-07 18:42:09 +0900

    debugging

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,8 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 user_name="momotaro98"
 
-ls -la
-git submodule status
-cd themes/resume
-ls -la
-cd ../..
-
 echo 'Go To Public folder'
 cd public
-
-ls -la
 
 echo 'Set Git configurations'
 git config --local user.name $user_name
@@ -24,7 +16,7 @@ git config --local user.email ${EMAIL_ADDRESS}
 git remote set-url origin https://$user_name:${DEPLOY_TOKEN}@github.com/$user_name/$user_name.github.io.git
 
 echo 'Add changes to git.'
-git checkout -b master
+git checkout master
 git add .
 
 echo 'Commit changes'
@@ -40,7 +32,7 @@ git push origin HEAD
 
 echo 'Back from public'
 cd ..
-git checkout -b master
+git checkout master
 git add public
 git commit -m "Deploy public `date`"
 git push origin HEAD
